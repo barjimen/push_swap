12710970fb775157f2ab5e680b57657088782dce
barjimen <barby.jimenez17@gmail.com>
2024-09-04 21:55:03 +0200

    order 3 nb

--- src/order_nb.c
+++ src/order_nb.c
@@ -6,16 +6,39 @@
 /*   By: barjimen <barjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 00:13:30 by barjimen          #+#    #+#             */
-/*   Updated: 2024/09/04 00:39:59 by barjimen         ###   ########.fr       */
+/*   Updated: 2024/09/04 21:47:59 by barjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 
+void	order_three(t_stack *stack_src)
+{
+	int	nb_one;
+	int	nb_two;
+	int	nb_three;
+
+	nb_one = stack_src->content;
+	nb_two = stack_src->next->content;
+	nb_three = stack_src->next->next->content;
+	if (nb_one > nb_two && nb_one > nb_three)
+	{
+		swap(stack_src, 'a');
+		if (nb_three < nb_one && nb_three < nb_two)
+			rotate_reverse(&stack_src, 'a', 1);
+	}
+	else if (nb_two > nb_one && nb_two > nb_three)
+	{
+		rotate_reverse(&stack_src, 'a', 1);
+		if (nb_one > nb_two && nb_three > nb_two)
+			swap(stack_src, 'a');
+	}
+}
+
 void	order_nb(t_stack **stack_src)
 {
-	if (stack_size(*stack_src) <= 2)
+	if (stack_size(*stack_src) == 2)
 		swap(*stack_src, 'a');
-    if (stack_size(*stack_src) <= 3)
-    
+	if (stack_size(*stack_src) == 3)
+		order_three(*stack_src);
 }
